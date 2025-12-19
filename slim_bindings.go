9d767f75fdeed26167119968d2f082f322bd20b6
Mauro Sardara <msardara@cisco.com>
2025-12-19 17:12:20 +0100

    Update slim_bindings.go
    Signed-off-by: Mauro Sardara <msardara@cisco.com>

--- slim_bindings.go
+++ slim_bindings.go
@@ -2,10 +2,10 @@ package slim_bindings
 
 /*
 #cgo CFLAGS: -I${SRCDIR}
-#cgo linux,amd64 LDFLAGS: ${SRCDIR}/../../../../../.cache/slim-bindings/libslim_bindings.a -lm
-#cgo linux,arm64 LDFLAGS: ${SRCDIR}/../../../../../.cache/slim-bindings/libslim_bindings.a -lm
-#cgo darwin,amd64 LDFLAGS: ${SRCDIR}/../../../../../.cache/slim-bindings/libslim_bindings.a -Wl,-undefined,dynamic_lookup
-#cgo darwin,arm64 LDFLAGS: ${SRCDIR}/../../../../../.cache/slim-bindings/libslim_bindings.a -Wl,-undefined,dynamic_lookup
+#cgo linux,amd64 LDFLAGS: ${SRCDIR}/../../../../../../.cache/slim-bindings/libslim_bindings.a -lm
+#cgo linux,arm64 LDFLAGS: ${SRCDIR}/../../../../../../.cache/slim-bindings/libslim_bindings.a -lm
+#cgo darwin,amd64 LDFLAGS: ${SRCDIR}/../../../../../../.cache/slim-bindings/libslim_bindings.a -Wl,-undefined,dynamic_lookup
+#cgo darwin,arm64 LDFLAGS: ${SRCDIR}/../../../../../../.cache/slim-bindings/libslim_bindings.a -Wl,-undefined,dynamic_lookup
 #include <slim_bindings.h>
 */
 import "C"
